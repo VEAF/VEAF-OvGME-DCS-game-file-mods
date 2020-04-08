74d81a1b0a86db338b61d77d88e36b3c4cf954f4
David Pierron <dpierron@gmail.com>
2020-04-08 19:32:08 +0200

    Added mods
    - Optimization - Shaders - Gryzor shadows removal
    - Optimization - Shaders - Mustang flat shadows removal and additional shaders

---
+++ xOptimization_-_Shaders_-_Gryzor_shadows_removal/Optimization - Shaders - Gryzor shadows removal/Config/Effects/shadows.lua
@@ -0,0 +1,65 @@
+shadows = 
+{
+
+	Nevada = {
+
+		{	--flat only
+			1,
+			{0, 0, 0, 0, 0, 7000},	
+			{0, 0, 0, 0, 0, 7000}	
+		},
+		{	--low
+			3072,							-- size of shadowmap
+			{0, 0.02, 40, 60, 80, 0},		-- split distances outside
+			{0, 0.02, 40, 60, 80, 0}		-- split distances in cockpit
+		},
+		{	--medium
+			3072,							-- size of shadowmap
+			{0, 0.02, 40, 60, 80, 0},		-- split distances outside
+			{0, 0.02, 40, 60, 80, 0}		-- split distances in cockpit
+		},
+		{	--high
+			3072,							-- size of shadowmap
+			{0, 0.02, 40, 60, 80, 0},		-- split distances outside
+			{0, 0.02, 40, 60, 80, 0}		-- split distances in cockpit
+		},
+		{	--ultra
+			3072,							-- size of shadowmap
+			{0, 0.02, 40, 60, 80, 0},		-- split distances outside
+			{0, 0.02, 40, 60, 80, 0}		-- split distances in cockpit
+		},
+
+	},
+
+	default = {
+
+		{	--flat only
+			1,
+			{0, 0, 0, 0, 0, 7000},	
+			{0, 0, 0, 0, 0, 7000}	
+		},
+		{	--low
+			3072,							-- size of shadowmap
+			{0, 0.02, 40, 60, 80, 0},		-- split distances outside
+			{0, 0.02, 40, 60, 80, 0}		-- split distances in cockpit
+		},
+		{	--medium
+			3072,							-- size of shadowmap
+			{0, 0.02, 40, 60, 80, 0},		-- split distances outside
+			{0, 0.02, 40, 60, 80, 0}		-- split distances in cockpit
+		},
+		{	--high
+			3072,							-- size of shadowmap
+			{0, 0.02, 40, 60, 80, 0},		-- split distances outside
+			{0, 0.02, 40, 60, 80, 0}		-- split distances in cockpit
+		},
+		{	--ultra
+			3072,							-- size of shadowmap
+			{0, 0.02, 40, 60, 80, 0},		-- split distances outside
+			{0, 0.02, 40, 60, 80, 0}		-- split distances in cockpit
+		},
+
+	},
+
+
+};
---
+++ xOptimization_-_Shaders_-_Gryzor_shadows_removal/README.txt
@@ -0,0 +1,12 @@
+Supprime _toutes_ les ombres lointaines (>500m) sauf celles du terrain (montagnes); celles dans le cockpit sont améliorées.
+Choisir la qualité d'ombrage désirée dans "Shadows".
+10% d'amélioration sur les fps moyennes avec ce mod.
+
+---------------------------------------
+-- Tested with DCS World 2.5.6.45915 --
+---------------------------------------
+
+Updated by Zip, made by Gryzor (https://forums.eagle.ru/showpost.php?p=4276907&postcount=25)
+
+Zip.
+http://www.veaf.org
