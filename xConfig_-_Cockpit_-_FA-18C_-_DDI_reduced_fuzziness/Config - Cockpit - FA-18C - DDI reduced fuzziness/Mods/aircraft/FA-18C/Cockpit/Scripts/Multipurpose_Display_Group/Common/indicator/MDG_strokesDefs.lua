01e52da882a93064ed4a919fc046345955963cf4
David Pierron <dpierron@gmail.com>
2020-05-14 11:39:15 +0200

    Added "Config - Cockpit - FA-18C - DDI reduced fuzziness"

--- xConfig_-_Cockpit_-_FA-18C_-_DDI_reduced_fuzziness/Config - Cockpit - FA-18C - DDI reduced fuzziness/Mods/aircraft/FA-18C/Cockpit/Scripts/Multipurpose_Display_Group/Common/indicator/MDG_strokesDefs.lua
+++ xConfig_-_Cockpit_-_FA-18C_-_DDI_reduced_fuzziness/Config - Cockpit - FA-18C - DDI reduced fuzziness/Mods/aircraft/FA-18C/Cockpit/Scripts/Multipurpose_Display_Group/Common/indicator/MDG_strokesDefs.lua
@@ -1,9 +1,8 @@
-
-stroke_thickness  = 0.5
-stroke_fuzziness  = 0.3
+stroke_thickness = 0.9
+stroke_fuzziness = 0.25
 
 -- Currently is used for DMC generated fonts black outline
-DMC_outline_thickness = stroke_thickness * 3
-DMC_outline_fuzziness = stroke_fuzziness * 1.1
-DMC_stroke_thickness  = 0.65
-DMC_stroke_fuzziness  = 0.42
+DMC_outline_thickness = stroke_thickness * 3.0
+DMC_outline_fuzziness = stroke_fuzziness * 8.0
+DMC_stroke_thickness = 1.0
+DMC_stroke_fuzziness = 0.3
